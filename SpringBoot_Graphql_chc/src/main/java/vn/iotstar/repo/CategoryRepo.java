be399b863c6f555261eed0491a1e87dbb3053256
nauct7 <thanhnau25@gmail.com>
2025-09-28 12:24:24 +0700

    dlt

--- SpringBoot_Graphql_chc/src/main/java/vn/iotstar/repo/CategoryRepo.java
+++
@@ -1,7 +0,0 @@
-package vn.iotstar.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import vn.iotstar.model.Category;
-
-public interface CategoryRepo extends JpaRepository<Category, Long> {}
--- SpringBoot_Graphql_chc/src/main/java/vn/iotstar/repo/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package vn.iotstar.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import vn.iotstar.model.User;
-
-public interface UserRepo extends JpaRepository<User, Long> {}
